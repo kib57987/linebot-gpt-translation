df11c695db29477b4390a5104f988f19b5a14641
kib57987 <kib57987@gmail.com>
2023-11-21 22:36:09 +0800

    Update app.py 777888

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from openai import OpenAI
 from linebot import LineBotApi, WebhookHandler
 from linebot.v3.webhook import WebhookParser
 from linebot.exceptions import InvalidSignatureError
-from linebot.models import MessageEvent
+from linebot.models import MessageEvent, TextSendMessage
 import os
 
 app = Flask(__name__)
